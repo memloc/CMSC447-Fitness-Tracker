457d8085b5bca6bf1e8cdaad4f5eb966bc8c9899
memloc <81724678+memloc@users.noreply.github.com>
2024-10-02 17:02:42 -0400

    run prettier

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Dependencies
 
 Install node and ensure that the version is greater or equal to v20.9.0 (LTS). If your version is before version v20.9.0 then you can update node or install `n` via `npm` to manage your node version and run the following commands with administrator privilege.
+
 ```sh
 npm cache clean -f
 npm install -g n
@@ -12,6 +13,7 @@ n 20.9.0
 ## Building
 
 1. Clone the repository and install node modules for project root
+
 ```sh
 git clone git@github.com:memloc/CMSC447-Fitness-Tracker.git
 cd CMSC447-Fitness-Tracker
@@ -19,17 +21,20 @@ npm install
 ```
 
 2. Run the following setup script to install client and server dependencies
+
 ```
 npm run setup
 ```
 
 3. Copy your `config.env` to the server directory with your atlas db connection string resembling the following format
+
 ```
 ATLAS_URI=mongodb+srv://<username>:<password>@<cluster>.<projectId>.mongodb.net/employees?retryWrites=true&w=majority
 PORT=5050
 ```
 
 4. Run the following from the project root directory to start the client and server
+
 ```
 npm run dev
 ```
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,49 +1,46 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from "./App";
-import Record from "./components/Record";
-import RecordList from "./components/RecordList";
-import "./index.css";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Record from './components/Record'
+import RecordList from './components/RecordList'
+import './index.css'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <RecordList />,
-      },
-    ],
-  },
-  {
-    path: "/edit/:id",
-    element: <App />,
-    children: [
-      {
-        path: "/edit/:id",
-        element: <Record />,
-      },
-    ],
-  },
-  {
-    path: "/create",
-    element: <App />,
-    children: [
-      {
-        path: "/create",
-        element: <Record />,
-      },
-    ],
-  },
-]);
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <RecordList />,
+            },
+        ],
+    },
+    {
+        path: '/edit/:id',
+        element: <App />,
+        children: [
+            {
+                path: '/edit/:id',
+                element: <Record />,
+            },
+        ],
+    },
+    {
+        path: '/create',
+        element: <App />,
+        children: [
+            {
+                path: '/create',
+                element: <Record />,
+            },
+        ],
+    },
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
