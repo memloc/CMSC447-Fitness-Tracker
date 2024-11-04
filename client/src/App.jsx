8c326a0e5d51dc78f7880bbbb042d4224b7a2845
Dhanush Bharadwaj <iz67933@umbc.edu>
2024-11-04 12:12:02 -0500

    Updated the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import { Outlet } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'
+import LoginPage from './components/LoginPage'
+import Calender from './components/Calender'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './components/Homepage'
 
 const App = () => {
     return (
-        <div className="w-full p-6">
-            <Navbar />
-            <Outlet />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/calender" element={<Calender />} />
+                <Route path="*" element="404 Page Not Found" />
+            </Routes>
+        </BrowserRouter>
     )
 }
 export default App
