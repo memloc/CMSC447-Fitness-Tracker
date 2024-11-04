77f3e191dcc2d5c626b4c07a22a6cc3efe3f8ef8
Dhanush Bharadwaj <iz67933@umbc.edu>
2024-11-04 00:57:48 -0500

    Add/update files in the components folder

---
+++ client/src/components/Calender.jsx
@@ -0,0 +1,194 @@
+
+import React, { useState, getEffect, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import Workout from './Workout'
+
+const localizer = momentLocalizer(moment)
+
+const MyCalendar = () => {
+    const [events, setEvents] = useState([])
+    const [selectedWorkout, setSelectedWorkout] = useState(null)
+    const navigate = useNavigate()
+
+    // Fetches the WorkoutEvents from the database.
+    useEffect(() => {
+        async function getWorkoutEvents() {
+            const response = await fetch(
+                `http://localhost:5050/workout_events/`
+            )
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`
+                console.error(message)
+                return
+            }
+            const workouts = await response.json()
+
+            /* TODO: Remove console.log for production builds */
+            for (const workout of workouts) {
+                console.log(`Fetched: Workout ObjectID: ${workout._id}\n`)
+            }
+
+            setEvents(workouts)
+        }
+        getWorkoutEvents()
+        return
+    }, [events.length])
+
+    /* TODO: Maybe cancel button on workout event create? */
+    /* handleEventCreateCancel() ... // Delete the blank workout from db */
+
+    const handleEventCreate = ({ start, end }) => {
+        const title = window.prompt('Enter a title for your workout:')
+        if (title) {
+            const newWorkout = {
+                id: events.length,
+                isNew: 1,
+                start,
+                end,
+                title,
+                sets: [],
+            }
+
+            // Must also post the blank workout being created to the DB
+            // to fix issue of disappearing events during creation
+            handleSaveWorkout(newWorkout)
+
+            setEvents([...events, newWorkout])
+            setSelectedWorkout(newWorkout)
+        }
+    }
+
+    const handleEventClick = (event) => {
+        /* TODO: Remove console.log for production builds */
+        console.log(
+            `Clicked existing workout`,
+            `\nid=${event.id}`,
+            `\nObjectID=${event._id}\n`,
+            `${JSON.stringify(event)}`
+        )
+
+        setSelectedWorkout(event)
+    }
+
+    async function handleSaveWorkout(updatedWorkout) {
+        // Get the updatedWorkout object id
+        try {
+            let response
+            if (updatedWorkout.isNew) {
+                /* TODO: Disable console.log in production builds */
+                console.log(
+                    `Attempting to save new workout`,
+                    `\nid=${updatedWorkout.id}`,
+                    `\nObjectID=${updatedWorkout._id}\n`,
+                    `${JSON.stringify(updatedWorkout)}`
+                )
+
+                // Set the workout.IsNew to false since its now being saved
+                updatedWorkout.isNew = false
+
+                // Workout is new so POST the workout to /workout_events
+                response = await fetch(`http://localhost:5050/workout_events`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(updatedWorkout),
+                })
+            } else {
+                /* TODO: Disable console.log in production builds */
+                console.log(
+                    `Attempting to patch existing workout`,
+                    `\nid=${updatedWorkout.id}`,
+                    `\nObjectID=${updatedWorkout._id}\n`,
+                    `${JSON.stringify(updatedWorkout)}`
+                )
+
+                // Update is not new so PATCH the workout_event of id
+                response = await fetch(
+                    `http://localhost:5050/workout_events/${updatedWorkout._id}`,
+                    {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(updatedWorkout),
+                    }
+                )
+            }
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+        } catch (error) {
+            console.error(
+                `A problem occurred with fetching workout event: `,
+                error
+            )
+        } finally {
+            const newEvents = events.map((event) =>
+                event.id === updatedWorkout.id ? updatedWorkout : event
+            )
+            setEvents(newEvents)
+            setSelectedWorkout(null)
+            navigate('/')
+        }
+    }
+
+    async function handleDeleteWorkout(workout) {
+        // Try to delete the workout event from the db
+        try {
+            /* TODO: Disable console.log in production builds */
+            console.log(
+                `Attempting to delete workout`,
+                `\nid=${workout.id}`,
+                `\nObjectID=${workout._id}\n`,
+                `${JSON.stringify(workout)}`
+            )
+
+            const response = await fetch(
+                `http://localhost:5050/workout_events/${workout._id}`,
+                {
+                    method: 'DELETE',
+                }
+            )
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+        } catch (error) {
+            console.error(
+                `A problem occurred with deleting workout event:`,
+                error
+            )
+        } finally {
+            const newEvents = events.filter((event) => event.id !== workout.id)
+            setEvents(newEvents)
+            setSelectedWorkout(null)
+            navigate('/')
+        }
+    }
+
+    return (
+        <div style={{ height: '500px' }}>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                selectable
+                onSelectSlot={handleEventCreate}
+                onSelectEvent={handleEventClick}
+            />
+            {selectedWorkout && (
+                <Workout
+                    workout={selectedWorkout}
+                    onSave={handleSaveWorkout}
+                    onDelete={handleDeleteWorkout}
+                />
+            )}
+        </div>
+    )
+}
+
+export default MyCalendar
---
+++ client/src/components/Workout.jsx
@@ -0,0 +1,92 @@
+
+import React, { useState } from 'react'
+
+const Workout = ({ workout, onSave, onDelete }) => {
+    const [sets, setSets] = useState(workout.sets || [])
+    const [name, setName] = useState(workout.title)
+    const [isNew, setIsNew] = useState(true)
+
+    const addSet = () => {
+        setSets([...sets, { weight: '', reps: '' }])
+    }
+
+    const updateSet = (index, field, value) => {
+        const newSets = [...sets]
+        newSets[index][field] = value
+        setSets(newSets)
+    }
+
+    const removeSet = (index) => {
+        const newSets = sets.filter((_, i) => i !== index)
+        setSets(newSets)
+    }
+
+    const handleSave = () => {
+        onSave({ ...workout, title: name, sets })
+    }
+
+    const handleDelete = () => {
+        onDelete({ ...workout, title: name, sets })
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Workout Name"
+            />
+            {sets.map((set, index) => (
+                <div key={index}>
+                    <input
+                        type="number"
+                        value={set.weight}
+                        onChange={(e) =>
+                            updateSet(index, 'weight', e.target.value)
+                        }
+                        placeholder="Weight"
+                    />
+                    <input
+                        type="number"
+                        value={set.reps}
+                        onChange={(e) =>
+                            updateSet(index, 'reps', e.target.value)
+                        }
+                        placeholder="Reps"
+                    />
+                    <button
+                        className="bg-blue-800 text-white font-bold py-1 px-2 \
+                        rounded m-1"
+                        onClick={() => removeSet(index)}
+                    >
+                        Remove Set
+                    </button>
+                </div>
+            ))}
+            <button
+                className="bg-blue-800 text-white font-bold py-1 px-2 rounded \
+				ml-1 mr-1"
+                onClick={addSet}
+            >
+                Add Set
+            </button>
+            <button
+                className="bg-blue-800 text-white font-bold py-1 px-2 rounded \
+				ml-1 mr-1"
+                onClick={handleSave}
+            >
+                Save Workout
+            </button>
+            <button
+                className="bg-blue-800 text-white font-bold py-1 px-2 rounded \
+				ml-1 mr-1"
+                onClick={handleDelete}
+            >
+                Delete Workout
+            </button>
+        </div>
+    )
+}
+
+export default Workout
