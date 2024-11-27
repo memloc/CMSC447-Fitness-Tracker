9b07e8c8548b4125a388c7f0658d24f40501e017
RayanMW <145315889+RayanMW@users.noreply.github.com>
2024-11-27 15:24:57 -0500

    Updated UI element
    Theme follows the original concept. Darker UI with a horizontal layout.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,12 @@
-import './App.css'
 import React from 'react'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Calendar from './components/Calendar'
-import HomePage from './components/HomePage'
-import LoginPage from './components/LoginPage'
-
+import FitnessCalendar from './components/FitnessCalendar'
 
 function App() {
-    return (
-		<BrowserRouter>
-            <Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/calendar" element={<Calendar />} />
-				<Route path="*" element="404 Page Not Found" />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <div className="min-h-screen bg-black">
+      <FitnessCalendar />
+    </div>
+  )
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,10 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
-import Workout from './components/Workout'
-import Calendar from './components/Calendar'
-import LoginPage from './components/LoginPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-		<App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
